37ed6c56418667dfa0eb536f47e185879083c760
Ullynacuaria <84741271+Ullynacuaria@users.noreply.github.com>
2022-06-17 15:33:10 -0500

    almost done

--- form.js
+++ form.js
@@ -1,3 +1,5 @@
+
+
 const form = document.getElementById('form')
 const username = document.getElementById('username')
 const email = document.getElementById('email')
@@ -11,10 +13,62 @@ form.addEventListener('submit', (e) => {
 
 });
 
+// get user value and trim any spaces 
 function getUserInputs(){
-    let usernameValue = username.value.trim()
-    let emailValue = email.value.trim()
-    let passwordValue = password.value.trim()
-    let password2Value = password2.value.trim()
+    let usernameValue = username.value.trim();
+    let emailValue = email.value.trim();
+    let passwordValue = password.value.trim();
+    let password2Value = password2.value.trim();
 
+// show error and add error class
+    if(usernameValue === ''){
+        setErrorFor(username, 'username cannot be blank');
+    }else{
+        setSuccessFor(username);
+    }
+
+    if(emailValue === ''){
+        setErrorFor(email, 'email cannot be blank');
+    }else if(!isEmail(emailValue)){
+        setErrorFor(email, 'Not a valid Email');
+    }else{
+        setSuccessFor(email)
+    }
+
+    if(passwordValue === ''){
+        setErrorFor(password, 'cannot be blank')
+    }else{
+        setSuccessFor(password)
+    }
+
+    if(passwordValue === ''){
+        setErrorFor(password2, 'cannot be blank');
+    }else if (passwordValue !== password2){
+        setErrorFor(password2, 'Password does not match');
+    }else{
+        setSuccessFor(password2)
+    }
+// Show a sucess message
+}
+
+function setErrorFor(input, message) {
+    const formControl = input.parentElement; // .form-control
+    const small = formControl.querySelector('small') // add error message inside the small tag
+    small.innerText = message;
+
+    //add error class
+    formControl.className = 'form-control error'
+    
+}
+//
+function setSuccessFor(input){
+    const formControl = input.parentElement;
+    formControl.className = 'form-control success'
+}
+// Email validadtion
+function isEmail(email){
+ if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email))
+  {
+    return (true)
+  }
 }
